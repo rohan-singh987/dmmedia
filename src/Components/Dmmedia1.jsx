313d24cdff22634b9ddba44f6f6a3761a0dea7fe
Rohan Singh <rohannsingh98765@gmail.com>
2021-12-03 12:38:06 +0530

    digitalMedia

---
+++ src/Components/Dmmedia1.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './dmmedia.css'
+
+const Dmmedia1 = () => {
+    return (
+        <div className="container py-5">
+            <div className="d-xl-flex">
+                <div>
+                    <img src="image/Group 991.png" alt="" className="img-fluid m-2" />
+                </div>
+                <div className="container ddm-audit">
+                    <h1>Marketing Audit and Recommendations</h1>
+                    <p>Sometimes the most complex challenge requires the simplest of solutions - the trick is to possess the ability to recognise the solution.  With the rare ability to identify, recommend and simultaneously mitigate challenges using in depth analytics and operational evaluation, Corporality Global conducts audits across your marketing landscape to provide enjoinders that boost business goals and growth.</p>
+
+                    
+                    <div className="ddm-audit-strategies">
+                    <h3>Our strategies will :</h3>
+                    <p>
+                        <img src="image/Group 985.png" alt="" className="img-fluid mx-2" />
+                        Streamline your traditional and digital marketing management processes
+                    </p>
+                    <p>
+                        <img src="image/Group 985.png" alt="" className="img-fluid mx-2" />
+                        Address inefficiencies while augmenting efficacy 
+                    </p>
+                    <p>
+                        <img src="image/Group 985.png" alt="" className="img-fluid mx-2" />
+                        Cut down on unnecessary costs
+                    </p>
+                    <p>
+                        <img src="image/Group 985.png" alt="" className="img-fluid mx-2" />
+                        Cut down on unnecessary costs
+                    </p>
+                    </div>
+
+                    <p>
+                    Get a fresh perspective with the help of technology to establish the power of your brand. It is extremely crucial to substantiate your marketing spend to keep your overall business performance in check.
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Dmmedia1
---
+++ src/Components/Dmmedia2.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Dmmedia2 = () => {
+    return (
+
+        <div className="d-xl-flex container">
+            <div className="m-5 ddm-audit">
+                    <h1>Key objectives</h1>
+                    <p>
+                    The key objective is to align all short-term and long-term business goals in order of their priority and accomplish them over the next two to three years. Create the ideal buyer persona, identify competition and describe products and services, to map out a detailed inventory encompassing your marketing assets.
+                    </p>
+
+                    
+                    <div className="ddm-audit-strategies">
+                    <p>
+                        <img src="image/Group 985.png" alt="" className="img-fluid mx-2" />
+                        Augmenting company visibility
+                    </p>
+                    <p>
+                        <img src="image/Group 985.png" alt="" className="img-fluid mx-2" />
+                        Increasing the size of the audience with corporate or business level strategy 
+                    </p>
+                    <p>
+                        <img src="image/Group 985.png" alt="" className="img-fluid mx-2" />
+                        Differentiating you from competition
+                    </p>
+                    <p>
+                        <img src="image/Group 985.png" alt="" className="img-fluid mx-2" />
+                        Market share increase and maintenance
+                    </p>
+                    <p>
+                        <img src="image/Group 985.png" alt="" className="img-fluid mx-2" />
+                        Generating qualified leads with effective lead generation strategy
+                    </p>
+                    <p>
+                        <img src="image/Group 985.png" alt="" className="img-fluid mx-2" />
+                        Customer engagement and usage
+                    </p>
+                    </div>
+
+                    <p>
+                    A detailed marketing audit under your belt helps you provide marching orders that are strategically aligned towards your marketing goals.
+                    </p>
+                </div>
+
+                <div>
+                    <img src="image/cuate.png" className="img-fluid m-5" alt="" />
+                </div>
+        </div>
+    )
+}
+
+export default Dmmedia2
